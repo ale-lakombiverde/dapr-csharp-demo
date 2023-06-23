a32ce6b8b2b7b9d02af158c96d33cb7a5112172d
La kombi verde <103608213+ale-lakombiverde@users.noreply.github.com>
2023-06-23 02:18:14 +0000

    se elimina proyecto anterior

--- Orders.Processor/Models/OrderModel.cs
+++
@@ -1,7 +0,0 @@
-public class OrderModel
-{
-    public Guid OrderId { get; set; } = Guid.NewGuid();
-    public string Reference { get; set; } = string.Empty;
-    public int Quantity { get; set; }
-    public DateTime CreatedOn { get; set; }
-}
